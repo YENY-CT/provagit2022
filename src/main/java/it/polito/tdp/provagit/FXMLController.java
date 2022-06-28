a0cf9b0aa79d413cc586855d94f0d79e4a6ca95f
hp <hp@LAPTOP-HBELL0EB.wind3.hub>
2022-06-28 08:13:50 +0200

    try

--- src/main/java/it/polito/tdp/provagit/FXMLController.java
+++ src/main/java/it/polito/tdp/provagit/FXMLController.java
@@ -14,7 +14,7 @@ public class FXMLController implements Initializable {
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("try2");
+        System.out.println("try3");
         label.setText("Buongionro a tt/e !");
     }
     
